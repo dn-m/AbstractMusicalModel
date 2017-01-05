dd1876649a5f3c5fc7944ceb616bad98ee73c837
James <jbean46@gmail.com>
2017-01-05 18:53:26 +0100

    Clarify Attribution model

--- AbstractMusicalModel/Attribution.swift
+++ AbstractMusicalModel/Attribution.swift
@@ -6,4 +6,15 @@
 //
 //
 
+/// A mapping between a generic `Attribute` and an atomic `Event`.
+///
+/// - note: Consider splitting this out between `AtomicAttribution` and `SpanningAttribution`.
 typealias Attribution <Attribute> = Dictionary<Event, Attribute>
+
+/// The unique identifier for a given `Attribution`.
+///
+/// - note: This could be a `String` or an `Int`.
+typealias AttributeIdentifier = String
+
+/// Dictionary mapping `AttributionIdentifier` values to `Attribution` values.
+typealias AttributionModel <Attribute> = Dictionary<AttributeIdentifier, Attribution>
