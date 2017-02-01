53bcab348ea6365bc60291fb3f25523ceb7245a8
James <jbean46@gmail.com>
2017-02-01 12:07:38 -0500

    Update: Entity -> Context, etc

--- AbstractMusicalModel/AttributionCollection.swift
+++ AbstractMusicalModel/AttributionCollection.swift
@@ -5,8 +5,8 @@
 //  Created by James Bean on 1/6/17.
 //
 //
-
-/// Mapping of an identifier of an `Attribution` to an `Attribution`.
-public typealias AttributionCollection <Attribute> = Dictionary<
-    AttributionIdentifier, Attribution<Attribute>
->
+//
+///// Mapping of an identifier of an `Attribution` to an `Attribution`.
+//public typealias AttributionCollection <Attribute> = Dictionary<
+//    AttributionIdentifier, Attribution<Attribute>
+//>
