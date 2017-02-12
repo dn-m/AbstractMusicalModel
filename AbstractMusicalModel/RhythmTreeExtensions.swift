e0cc10cd99ef072a4011678ebb8af89b984eefbd
James <jbean46@gmail.com>
2017-02-12 00:58:42 -0500

    Get frameworks on same page

---
+++ AbstractMusicalModel/RhythmTreeExtensions.swift
@@ -0,0 +1,19 @@
+//
+//  RhythmTreeExtensions.swift
+//  AbstractMusicalModel
+//
+//  Created by James Bean on 2/8/17.
+//
+//
+
+import Rhythm
+
+extension RhythmTree {
+    
+    public func attaching(_ events: [Model.Event]) throws -> RhythmTree {
+        
+        // 
+        
+        return self
+    }
+}
